70290479e1dc34a4c2fa9ef38ff4f0de61d18629
Ijas Ahammed <ahammedijas.official@gmail.com>
2024-03-17 10:57:22 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Container(
+          child: const Center(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
